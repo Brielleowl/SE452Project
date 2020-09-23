b9bb216cf2c0ecfb1332020642efd0a33d2b7fa6
Brielleowl <luoxiaofo619@gmail.com>
2020-09-22 23:14:51 -0500

    add luggage class

---
+++ LuggageEye/demo/src/main/java/edu/depaul/cdm/se452/demo/Luggage/Luggage.java
@@ -0,0 +1,28 @@
+package edu.depaul.cdm.se452.demo.Luggage;
+
+import lombok.Data;
+
+@Data
+public class Luggage {
+    private long luggageID;
+    private int allowance;
+    private int maxWeight;
+    private String status;
+
+    public void makePayment() {
+
+
+    }
+
+    public long createLuggageID() {
+
+
+    }
+
+    public int calculateAmount() {
+
+        
+    }
+
+
+}
